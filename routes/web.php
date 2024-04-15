589bdf8b10fdaa515997a634f73f212be34b8803
Le Tien Hai <tienhaile488@gmail.com>
2024-04-15 16:57:52 +0700

    feat: [feature/account-setting] handle redirect login

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
-use Symfony\Component\HttpKernel\Profiler\Profile;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,13 +33,9 @@ Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 
 Route::post('/login', [LoginController::class, 'login'])->name('post_login');
 
-Route::prefix('')->middleware('auth')->group(function () {
+Route::prefix('admin')->middleware('auth')->name('admin.')->group(function () {
     Route::get('logout', [LoginController::class, 'logout'])->name('logout');
 
-    Route::get('/home', [HomeController::class, 'index'])->name('home');
-});
-
-Route::prefix('admin')->middleware('auth')->name('admin.')->group(function () {
     Route::get('', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::prefix('profile')->name('profile.')->group(function () {
